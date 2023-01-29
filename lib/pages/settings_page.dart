1fbf7b01f8f85839b80610b7363b5b5ea33e7b26
sagar poudel <iamsagar099@gmail.com>
2023-01-29 21:50:39 +0545

    settisn ma upload ko form sakiyo

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,32 +1,127 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({Key? key}) : super(key: key);
-
   @override
-  State<SettingsPage> createState() => _SettingsPageState();
+  _SettingsPageState createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  final _formKey = GlobalKey<FormState>();
+  late String bookName;
+  late String bookAuthor;
+  late String bookDescription;
+
+  FilePickerResult ? result;
+  String ? fileName;
+  PlatformFile ? pickedFile;
+  bool isLoading = false;
+  File? fileToDisplay;
+
+  void pickFile() async{
+    try{
+
+      setState(() {
+        isLoading = true;
+      });
+      result = await FilePicker.platform.pickFiles(
+        type: FileType.any,
+        allowMultiple: false,
+      );
+
+      if(result!=null){
+        fileName = result!.files.first.name;
+        pickedFile = result!.files.first;
+        fileToDisplay = File(pickedFile!.path.toString());
+
+      }
+      setState(() {
+        isLoading = false;
+      });
+
+    } catch(e){
+      print(e.toString());
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upload Books'),
+      ),
+      body: Form(
+        key: _formKey,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 15.0),
-          child: Container(
-            child: Column(
-              children: const  [
-                 Text("Upload Book", style: TextStyle(fontWeight: FontWeight.bold),)
-              ],
-
-
-            ),
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Book Name'),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please enter book name';
+                  }
+                  return null;
+                },
+                onSaved: (value) => bookName = value!,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Book Author'),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please enter book author';
+                  }
+                  return null;
+                },
+                onSaved: (value) => bookAuthor = value!,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Book Description'),
+                validator: (value) {
+                  if (value==null) {
+                    return 'Please enter book description';
+                  }
+                  return null;
+                },
+                onSaved: (value) => bookDescription = value!,
+              ),
+             Container(
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   Center(
+                     child: isLoading
+                     ? CircularProgressIndicator()
+                     :ElevatedButton(onPressed: (){
+                       pickFile();
+                     }, child: Text("Upload a Book")),
+                   ),
+                   if(pickedFile!=null)
+                     SizedBox(height: 300,width: 400, child: Image.file(fileToDisplay!)),
+                 ],
+               ),
+             ),
+              ElevatedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    print(bookName);
+                    print(bookAuthor);
+                    print(bookDescription);
+                    // Submit the form and perform desired actions
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
-
     );
-
   }
 }
